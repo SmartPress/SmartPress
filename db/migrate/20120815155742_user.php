7b5fb18df425be42898e5ee663eb40ee3aafa46f
Zachary Quintana <zach.quintana@gmail.com>
2012-09-05 11:37:52 -0700

    Migration updates

--- db/migrate/20120815155742_user.php
+++ db/migrate/20120815155742_user.php
@@ -7,6 +7,7 @@ class User extends Migration {
 	public function change() {
 		$this->create_table("users", function() {
 			$this->string("username");
+			$this->string("email");
 			$this->string("password_hash");
 			$this->integer("group_id");
 
