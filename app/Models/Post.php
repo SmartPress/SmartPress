86f66762b72d7952875caad87328753174b78e43
Zachary Quintana <zach.quintana@gmail.com>
2013-01-15 17:38:28 -0800

    Adding consts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,16 @@ class Post extends \Speedy\Model\ActiveRecord {
 	const PostType	= 'post';
 	
 	private static $_statuses = ['Select One', 'Draft', 'Publish'];
+
+	public static $homeTypes = [
+		'Select One',
+		'Blog Roll',
+		'Single Page'
+	];
+
+	const BlogRollHomeType = 1;
+
+	const SinglePageHomeType = 2;
 	
 	private $_custom_data;
 	
